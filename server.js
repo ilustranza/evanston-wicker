7610f5ae9ab08e6b4e70adcabbdef3cf79fb580a
Alonso Solano <l.alonsosolano@gmail.com>
2020-07-31 11:21:15 -0500

    https logs

--- server.js
+++ server.js
@@ -7,6 +7,27 @@ const port = process.env.PORT || 3000;
 
 const sgMail = require('@sendgrid/mail');
 
+app.use((req, res, next) => {
+
+  console.log("---------------")
+  console.log(req.headers.host)
+  console.log(req.headers['x-forwarded-proto'])
+  console.log(req.secure)
+
+  if (process.env.NODE_ENV === 'production') {
+
+    if (req.headers.host === 'evanston-wicker.herokuapp.com')
+      return res.redirect(301, 'https://www.evanston-wicker.com');
+    if (req.headers['x-forwarded-proto'] !== 'https' || !req.secure)
+      return res.redirect('https://' + req.headers.host + req.url);
+    else
+      return next();
+    
+  } else
+    return next();
+
+});
+
 app.use(favicon(__dirname + '/build/favicon.ico'));
 app.use(express.static(__dirname));
 app.use(express.static(path.join(__dirname, 'build')));
@@ -23,18 +44,6 @@ app.use(bodyParser.urlencoded({ extended: true }));
 //     res.redirect('https://' + req.headers.host + req.url);
 //   }
 // });
-
-app.use((req, res, next) => {
-  if (process.env.NODE_ENV === 'production') {
-    if (req.headers.host === 'evanston-wicker.herokuapp.com')
-      return res.redirect(301, 'https://www.evanston-wicker.com');
-    if (req.headers['x-forwarded-proto'] !== 'https')
-      return res.redirect('https://' + req.headers.host + req.url);
-    else
-      return next();
-  } else
-    return next();
-});
 
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
@@ -56,6 +65,13 @@ app.get('/*', function (req, res) {
 });
 
 var server = app.listen(port, function() {
+
+  console.log(">>>>>>>>>>>>>>>>")
+  console.log(req.headers.host + req.url)
+  console.log(req.headers['x-forwarded-proto'])
+  console.log(req.secure)
+
+
   console.log('Server is up!');
   console.log('Listening on port %d', server.address().port);
 });
